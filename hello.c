e3a66dcb9f2a036c968e5353e115884dfc9eb0de
Aaldana0824 <59975303+Aaldana0824@users.noreply.github.com>
2020-01-16 13:36:23 -0600

    Create hello.c
    Added line 11 that prints my major

---
+++ hello.c
@@ -0,0 +1,13 @@
+/*
+Name: Austin Aldana
+Date: 2020/01/16
+Objective: To create code that will print my name
+*/
+
+#include <stdio.h>
+
+int main (void) {
+    printf("Austin Aldana\n");
+    printf("Computer Engineering\n");
+    return 0;
+}
